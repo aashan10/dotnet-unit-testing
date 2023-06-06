f9e360e3a8ce2178f38369e62d08c10ed0edc2c2
Aashan Ghimire <ashanghimire10@gmail.com>
2023-06-06 13:16:54 +0545

    Added ShapeValidatorService as a solution

---
+++ ShapeValidatorService/TriangleShapeValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace ShapeValidatorService;
+public class TriangleShapeValidator
+{
+
+    public bool IsValid(double a, double b, double c)
+    {
+        return (a + b + c == 180);
+    }
+
+}
